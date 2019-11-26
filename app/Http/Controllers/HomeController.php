d3908c6f73d29a42a142c2d6e8f062b7f80bb1e3
Hary Pham <harypham97@gmail.com>
2019-11-26 15:21:41 +0700

    init project

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Events\NewMessage;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        event(new NewMessage('hello world'));
+        return view('home');
+    }
+}
